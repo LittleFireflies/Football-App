5332c27a99ae6d42d14e25c07cd7adcbe2ea53e3
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-08-31 05:22:37 +0700

    init club list data

---
+++ app/src/main/java/com/littlefireflies/footballclub/Club.kt
@@ -0,0 +1,6 @@
+package com.littlefireflies.footballclub
+
+/**
+ * Created by widyarso.purnomo on 31/08/2018.
+ */
+data class Club(val name: String?, val image: Int?, val description: String?)
--- app/src/main/java/com/littlefireflies/footballclub/MainActivity.kt
+++ app/src/main/java/com/littlefireflies/footballclub/MainActivity.kt
@@ -5,8 +5,26 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val clubList: MutableList<Club> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initData()
+    }
+
+    private fun initData() {
+        val names = resources.getStringArray(R.array.club_name)
+        val images = resources.obtainTypedArray(R.array.club_image)
+        val descriptions = resources.getStringArray(R.array.club_description)
+
+        clubList.clear()
+
+        for (i in names.indices) {
+            clubList.add(Club(names[i], images.getResourceId(i, 0), descriptions[i]))
+        }
+
+        images.recycle()
     }
 }
