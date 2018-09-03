6d8ba5aeb4a3a68d89144fe9e5ebc946132db353
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-09-03 08:53:02 +0700

    design layout match schedule activity

---
+++ app/src/main/java/com/littlefireflies/footballclub/ui/MatchSchedule/MatchScheduleActivity.kt
@@ -0,0 +1,13 @@
+package com.littlefireflies.footballclub.ui.MatchSchedule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.littlefireflies.footballclub.R
+
+class MatchScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_schedule)
+    }
+}
