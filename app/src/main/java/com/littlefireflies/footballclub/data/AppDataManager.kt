e5ff107c0d7e36a3e28027a5fc3fd19ad8ddadf8
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-09-03 13:45:21 +0700

    inject data & network helper

--- app/src/main/java/com/littlefireflies/footballclub/data/AppDataManager.kt
+++ app/src/main/java/com/littlefireflies/footballclub/data/AppDataManager.kt
@@ -1,7 +1,11 @@
 package com.littlefireflies.footballclub.data
 
+import com.littlefireflies.footballclub.data.network.NetworkHelper
+import javax.inject.Inject
+
 /**
  * Created by widyarso.purnomo on 03/09/2018.
  */
-class AppDataManager : DataManager {
+class AppDataManager @Inject
+constructor(networkHelper: NetworkHelper) : DataManager {
 }
--- app/src/main/java/com/littlefireflies/footballclub/data/network/AppNetworkHelper.kt
+++ app/src/main/java/com/littlefireflies/footballclub/data/network/AppNetworkHelper.kt
@@ -1,7 +1,10 @@
 package com.littlefireflies.footballclub.data.network
 
+import javax.inject.Inject
+
 /**
  * Created by widyarso.purnomo on 03/09/2018.
  */
-class AppNetworkHelper : NetworkHelper {
+class AppNetworkHelper @Inject
+constructor(networkService: NetworkService) : NetworkHelper {
 }
