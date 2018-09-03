06bdab78535e59acd37d786aeee2a90ce3dbe4c5
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-09-03 11:32:27 +0700

    register application

---
+++ app/src/main/java/com/littlefireflies/footballclub/App.kt
@@ -0,0 +1,13 @@
+package com.littlefireflies.footballclub
+
+import android.app.Application
+
+/**
+ * Created by widyarso.purnomo on 03/09/2018.
+ */
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
