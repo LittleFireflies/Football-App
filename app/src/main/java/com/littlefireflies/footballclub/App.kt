2370396980e791aba44cd2170ede6611e7efee8f
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-09-03 13:33:36 +0700

    initialize dagger application component

--- app/src/main/java/com/littlefireflies/footballclub/App.kt
+++ app/src/main/java/com/littlefireflies/footballclub/App.kt
@@ -1,13 +1,24 @@
 package com.littlefireflies.footballclub
 
 import android.app.Application
+import com.littlefireflies.footballclub.di.component.ApplicationComponent
+import com.littlefireflies.footballclub.di.component.DaggerApplicationComponent
+import com.littlefireflies.footballclub.di.module.ApplicationModule
+import com.littlefireflies.footballclub.di.module.NetworkModule
 
 /**
  * Created by widyarso.purnomo on 03/09/2018.
  */
 class App: Application() {
 
+    lateinit var applicationComponent: ApplicationComponent
+
     override fun onCreate() {
         super.onCreate()
+        applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(ApplicationModule(this))
+                .networkModule(NetworkModule())
+                .build()
+        applicationComponent.inject(this)
     }
 }
