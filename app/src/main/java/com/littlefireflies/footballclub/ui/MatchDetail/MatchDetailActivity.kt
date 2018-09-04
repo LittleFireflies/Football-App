7d958ea05b6c4a6529d142638b24ae602be842a8
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-09-04 12:31:48 +0700

    design match detail layout

---
+++ app/src/main/java/com/littlefireflies/footballclub/ui/MatchDetail/MatchDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.littlefireflies.footballclub.ui.MatchDetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.littlefireflies.footballclub.R
+
+class MatchDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_detail)
+    }
+}
